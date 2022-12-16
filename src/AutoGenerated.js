fb113aa8aff9920355f0a23e5e3c2488c2fe3196
youwol-guillaume <greinisch@youwol.com>
2022-12-16 15:11:25 +0100

    ✨ Create (pseudo) AutoGenerated.js to gather constants from package.json

---
+++ src/AutoGenerated.js
@@ -0,0 +1,7 @@
+/**
+ * This file is NOT auto-generated yet, but it should be.
+ */
+export const jQuery = window['$_APIv3']
+export const potreePath =
+    '/api/assets-gateway/raw/package/QHlvdXdvbC9wb3RyZWU=/0.1.1-wip/build/potree'
+export const resourcePath = `${potreePath}/resources`
